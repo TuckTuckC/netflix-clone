71ddaaf6df161efe4721ea13d57c8b2736a04286
Tucker Craig <tuckermcraig@gmail.com>
2021-08-05 16:51:29 -0400

    added row component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Row from './conponents/Row';
 
 function App() {
   return (
     <div className="App">
-      
+      <Row title='NETFLIX ORIGINALS' />
+      <Row title='Trending Now' />
     </div>
   );
 };
