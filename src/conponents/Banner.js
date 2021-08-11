6a514bb9b02623b0b5bc635270a5f4321a08c877
Tucker Craig <tuckermcraig@gmail.com>
2021-08-11 18:34:01 -0400

    banner now gets random movie

--- src/conponents/Banner.js
+++ src/conponents/Banner.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from '../config/axios';
+import requests from '../config/requests';
 
 function Banner() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
 
-export default Banner
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+            ]);
+        };
+        fetchData();
+    }, []);
+
+    console.log(movie);
+
+    return (
+        <header>
+            
+        </header>
+    );
+};
+
+export default Banner;
--- src/conponents/Row.js
+++ src/conponents/Row.js
@@ -12,7 +12,6 @@ function Row({title, fetchUrl, isLargeRow}) {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
             setMovies(request.data.results);
-            console.log(request.data.results);
             return request;
         }
         fetchData()
