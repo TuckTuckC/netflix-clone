d82778978c6848485132b0b50e6cf514769fc488
Tucker Craig <tuckermcraig@gmail.com>
2021-08-11 18:17:56 -0400

    added banner component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Row from './conponents/Row';
+import Banner from './conponents/Banner';
 import requests from './config/requests';
 
 function App() {
   return (
     <div className="app">
+      <Banner />
       <Row 
         title='NETFLIX ORIGINALS' 
         fetchUrl={requests.fetchNetflixOriginals} 
