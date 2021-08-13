0f4b801f84dd82987a5bfcd472008a4145ed43dd
Tucker Craig <tuckermcraig@gmail.com>
2021-08-13 05:06:42 -0400

    added truncate

--- src/conponents/Banner.js
+++ src/conponents/Banner.js
@@ -19,6 +19,10 @@ function Banner() {
 
     console.log(movie);
 
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+    }
+
     return (
         <header 
             className='banner'
